26b739d236d317ccf68ce6f43d261d6594a54fe2
ehp2021 <emmahpaik@gmail.com>
2021-12-16 14:29:53 -0600

    fixed deploy-hello.ts

--- scripts/deploy-hello.ts
+++ scripts/deploy-hello.ts
@@ -2,18 +2,17 @@ import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 
 async function deploy() {
-  //doing the same thing as in the test, get the contract factory, gets all the info to deploy
-  const HelloWorld = await ethers.getContractFactory("HelloWorld");
-  // makes a json request, rpc it over to a netwk, network starts the contract, we wait for it to be deployed
-  const hello = await HelloWorld.deploy();
-  const hello.deployed(); //considered deployed
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const hello = await HelloWorld.deploy();
+    await hello.deployed();
 
-  return hello;
+    return hello;
 }
 
-//@ts-ignore
+// @ts-ignore
 async function sayHello(hello) {
-  console.log("Say Hello:", await hello.hello()); //then we run a fxn on it 
+    console.log("Say Hello:", await hello.hello());
 }
 
 deploy().then(sayHello);
+
